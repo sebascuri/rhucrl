9dc4deecea252bee267e5702fe0b835cf9943bf5
scuri <sebastian.curi@inf.ethz.ch>
2020-08-05 11:35:18 +0200

    Add Exception Catch for mujoco envs

--- rhucrl/environment/wrappers/__init__.py
+++ rhucrl/environment/wrappers/__init__.py
@@ -1,7 +1,11 @@
 """Python Script Template."""
 from .adversarial_wrapper import AdversarialWrapper
-from .mujoco_wrapper import MujocoAdversarialWrapper
 from .noisy_action_robust_wrapper import NoisyActionRobustWrapper
 from .probabilistic_action_robust_wrapper import ProbabilisticActionRobustWrapper
 from .reset_wrapper import ResetWrapper
 from .reward_wrapper import RewardWrapper
+
+try:
+    from .mujoco_wrapper import MujocoAdversarialWrapper
+except ModuleNotFoundError:
+    pass
