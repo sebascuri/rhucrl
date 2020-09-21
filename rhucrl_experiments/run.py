04ce6e40dff254a2f1cce572df6c836de465a430
scuri <sebastian.curi@inf.ethz.ch>
2020-09-21 21:44:39 +0200

    Save arguments

--- rhucrl_experiments/run.py
+++ rhucrl_experiments/run.py
@@ -35,6 +35,7 @@ def init_experiment(args, env_kwargs=None, **kwargs):
     if args.hallucinate:
         environment.add_wrapper(HallucinationWrapper)
 
+    agent.logger.save_hparams(arg_dict)
     return agent, environment
 
 
