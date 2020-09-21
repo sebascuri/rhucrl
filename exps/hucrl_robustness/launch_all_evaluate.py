be8e5cc9c2304813c77710be9366188c5cda2282
scuri <sebastian.curi@inf.ethz.ch>
2020-09-21 17:51:53 +0200

    Update test time

--- exps/hucrl_robustness/launch_all_evaluate.py
+++ exps/hucrl_robustness/launch_all_evaluate.py
@@ -4,7 +4,7 @@ import os
 
 from lsf_runner import init_runner, make_commands
 
-runner = init_runner("EvaluateKnownModel", wall_time=24 * 60, num_threads=4)
+runner = init_runner("EvaluateKnownModel", num_threads=4)
 cwd = os.path.dirname(os.path.realpath(__file__))
 script = "evaluate_nominal.py"
 
