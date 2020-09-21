082177fe5fd58994ad77b87044c99d72d4894260
scuri <sebastian.curi@inf.ethz.ch>
2020-09-21 17:26:01 +0200

    Add launch test

---
+++ exps/hucrl_robustness/launch_test.py
@@ -0,0 +1,19 @@
+"""Python Script Template."""
+
+import os
+
+from lsf_runner import init_runner, make_commands
+
+runner = init_runner("Known Model.", wall_time=24 * 60, num_threads=4)
+cwd = os.getcwd()
+script = "train_nominal.py"
+
+environments = ["MBHalfCheetah-v0"]
+
+commands = make_commands(
+    f"{cwd}/{script}",
+    base_args={"alpha": 0, "hallucinate": False, "train-episodes": 20},
+    common_hyper_args={"seed": [0], "protagonist-name": ["SAC"]},
+)
+
+runner.run(commands)
