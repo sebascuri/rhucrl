ec6b59add32bb492e77d2aea08cc0bdb9b74ba85
scuri <sebastian.curi@inf.ethz.ch>
2020-09-22 07:41:24 +0200

    Add other environments to train nominal

--- rhucrl_experiments/hucrl_robustness/launch_all_evaluate.py
+++ rhucrl_experiments/hucrl_robustness/launch_all_evaluate.py
@@ -8,8 +8,8 @@ runner = init_runner("EvaluateKnownModel", wall_time=24 * 60, num_threads=4)
 cwd = os.path.dirname(os.path.realpath(__file__))
 script = "evaluate_nominal.py"
 
-environments = [
-    "MBHalfCheetah-v0",
+ENVIRONMENTS = [
+    # "MBHalfCheetah-v0",
     "PendulumSwingUp-v0",
     "MBHopper-v0",
     "MBWalker2d-v0",
@@ -18,7 +18,11 @@ environments = [
 commands = make_commands(
     f"{cwd}/{script}",
     base_args={"alpha": 0, "hallucinate": False},
-    common_hyper_args={"seed": [0], "protagonist-name": ["SAC", "MPO", "PPO"]},
+    common_hyper_args={
+        "seed": [0],
+        "environment": ENVIRONMENTS,
+        "protagonist-name": ["SAC", "MPO", "PPO"],
+    },
 )
 
 commands += make_commands(
@@ -26,6 +30,7 @@ commands += make_commands(
     base_args={"alpha": 0, "hallucinate": [True, False]},
     common_hyper_args={
         "seed": [0],
+        "environment": ENVIRONMENTS,
         "protagonist-name": ["BPTT"],
         "num-steps": [1, 4, 8],
     },
@@ -36,6 +41,7 @@ commands += make_commands(
     base_args={"alpha": 0, "hallucinate": [True, False], "protagonist-name": "MVE"},
     common_hyper_args={
         "seed": [0],
+        "environment": ENVIRONMENTS,
         "base-agent": ["SAC", "MPO", "BPTT", "PPO"],
         "num-steps": [1, 4, 8],
     },
