f8b1f61de3b04696ca32edb6d60960c800179641
Sebastian Curi <sebascuri@gmail.com>
2020-12-15 20:00:22 -0300

    Update runners

--- applications/adversarial_rl/launch.py
+++ applications/adversarial_rl/launch.py
@@ -3,13 +3,13 @@ import os
 from lsf_runner import init_runner, make_commands
 
 
-runner = init_runner("AdversarialRL", wall_time=3 * 60, num_threads=2)
+runner = init_runner("AdversarialRL", wall_time=24 * 60, num_threads=2)
 cwd = os.path.dirname(os.path.realpath(__file__))
 script = "run.py"
 
-SEEDS = [0]
-ALPHAS = [1.0]  # , 2.0, 5.0, 10.0]
-ENVS = ["half_cheetah"]  # , "hopper", "inverted_pendulum", "reacher", "swimmer"]
+SEEDS = [0, 1, 2]
+ALPHAS = [1.0, 2.0, 5.0, 10.0]
+ENVS = ["half_cheetah", "hopper", "inverted_pendulum", "reacher", "swimmer", "walker"]
 ENVS = [f"config/envs/{env}.yaml" for env in ENVS]
 AGENTS = {
     "SAC": ["sac"],
@@ -18,7 +18,7 @@ AGENTS = {
     "DataAugmentation": ["hucrl", "hucrl3"],
     "RAP": ["ppo", "sac"],
     "RARL": ["ppo", "sac"],
-    "HRARL": ["mve"],
+    "HRARL": ["hucrl"],
     "MaxiMin": ["mve"],
     "BestResponse": ["mve"],
     "RHUCRL": ["mve", "mve3", "da"],
