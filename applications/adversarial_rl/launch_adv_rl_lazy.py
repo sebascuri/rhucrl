6beb18ff6a1ee39034cd1e4e37d51c8853fa37e8
scuri <sebastian.curi@inf.ethz.ch>
2021-01-15 16:38:11 +0100

    Add lazy launchers

---
+++ applications/adversarial_rl/launch_adv_rl_lazy.py
@@ -0,0 +1,41 @@
+"""Launch adversarial rl on lazy."""
+
+import os
+
+from lsf_runner import init_runner, make_commands
+
+cwd = os.path.dirname(os.path.realpath(__file__))
+script = "run_adv_rl.py"
+
+SEEDS = [0, 1, 2]
+ALPHAS = [1.0, 2.0, 5.0, 10.0]
+ENVS = ["half_cheetah", "hopper", "inverted_pendulum", "reacher", "swimmer", "walker"]
+ENVS = [f"config/envs/{env}.yaml" for env in ENVS]
+# I'M missing reacher with SAC and PPO
+AGENTS = {
+    # "SAC": ["sac"],
+    # "PPO": ["ppo"],
+    # "BPTT": ["hucrl_a", "hucrl_b", "hucrl_c"],
+    # "RAP": ["ppo"],
+    # "RARL": ["ppo"],
+    # "HRARL": ["hucrl_a", "hucrl_b", "hucrl_c"],
+    # "MaxiMin": ["hucrl_a", "hucrl_b", "hucrl_c", "sac", "ppo"],
+    # "BestResponse": ["hucrl_a", "hucrl_b", "hucrl_c"],
+    "RHUCRL": ["hucrl_a", "hucrl_b", "hucrl_c"]
+}
+
+for agent, agent_configs in AGENTS.items():
+    runner = init_runner(
+        f"AdversarialRL_{agent}", wall_time=24 * 60, num_threads=1, memory=4096
+    )
+    commands = []
+    for agent_config in agent_configs:
+        commands += make_commands(
+            script,
+            base_args={
+                "agent": agent,
+                "agent-config": f"config/agents/{agent_config}.yaml",
+            },
+            common_hyper_args={"seed": SEEDS, "env-config": ENVS, "alpha": ALPHAS},
+        )
+    runner.run_batch(commands)
---
+++ applications/domain_randomization/launch_dr_lazy.py
@@ -0,0 +1,30 @@
+"""Launch dr on lazy."""
+import os
+
+from lsf_runner import init_runner, make_commands
+
+cwd = os.path.dirname(os.path.realpath(__file__))
+script = "run_dr.py"
+
+SEEDS = [0, 1, 2]
+# ENVS = ["half_cheetah", "hopper", "inverted_pendulum", "reacher", "swimmer", "walker"]
+ENVS = ["half_cheetah", "inverted_pendulum", "reacher", "swimmer", "walker"]
+
+ENVS = [f"config/envs/{env}.yaml" for env in ENVS]
+AGENTS = {"RHUCRL": ["hucrl_a", "hucrl_b", "hucrl_c"]}
+
+for agent, agent_configs in AGENTS.items():
+    runner = init_runner(
+        f"DomainRandomizationRL_{agent}", wall_time=24 * 60, num_threads=1, memory=4096
+    )
+    commands = []
+    for agent_config in agent_configs:
+        commands += make_commands(
+            script,
+            base_args={
+                "agent": agent,
+                "agent-config": f"config/agents/{agent_config}.yaml",
+            },
+            common_hyper_args={"seed": SEEDS, "env-config": ENVS},
+        )
+    runner.run_batch(commands)
