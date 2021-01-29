07e96c5ff3177ba8eddc34bef1b077520cd34c08
scuri <sebastian.curi@inf.ethz.ch>
2021-01-29 09:16:28 +0100

    Remove baseline from launcher

--- applications/inverted_pendulum/launch.py
+++ applications/inverted_pendulum/launch.py
@@ -8,7 +8,7 @@ from lsf_runner import init_runner, make_commands
 cwd = os.path.dirname(os.path.realpath(__file__))
 script = "run.py"
 
-AGENTS = ["RHUCRL", "HUCRL", "baseline"]
+AGENTS = ["RHUCRL", "HUCRL"]  # , "baseline"]
 ROBUSTNESS = ["adversarial", "action", "parameter"]
 ALPHAS = [0.05, 0.1, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40]
 
