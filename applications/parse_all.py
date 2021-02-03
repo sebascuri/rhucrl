6ec64d13f60e318058d43505479f82edd576e999
scuri <sebastian.curi@inf.ethz.ch>
2021-02-03 14:00:33 +0100

    Add parse all helper

---
+++ applications/parse_all.py
@@ -0,0 +1,6 @@
+"""Python Script Template."""
+import os
+
+os.system("python adversarial_rl/parse_adv_rl.py")
+os.system("python action_robust/parse_arrl.py")
+os.system("python adversarial_rl/parse_dr.py")
