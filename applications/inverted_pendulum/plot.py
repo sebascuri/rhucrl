f3042d8aeea817ba8111f5a956fa3435f2beb27c
scuri <sebastian.curi@inf.ethz.ch>
2021-02-03 13:34:13 +0100

    Fix inverted_pendulum plot

--- applications/inverted_pendulum/plot.py
+++ applications/inverted_pendulum/plot.py
@@ -2,7 +2,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from applications.plotters import COLORS, LABELS, set_figure_params
+from applications.plotters import COLORS, LABELS, LINESTYLE, set_figure_params
 
 set_figure_params(serif=True, fontsize=10)
 fig, ax = plt.subplots(ncols=3, nrows=1, sharey="row")
@@ -13,7 +13,13 @@ agents = ["RHUCRL", "HUCRL", "baseline"]
 for agent in agents:
     df_ = df[df.agent == agent].sort_values("alpha")
     mean, std = df_["mean"], df_["std"]
-    ax[0].plot(alpha, mean, label=LABELS[agent], color=COLORS[agent])
+    ax[0].plot(
+        alpha,
+        mean,
+        label=LABELS[agent],
+        color=COLORS[agent],
+        linestyle=LINESTYLE[agent],
+    )
     ax[0].fill_between(
         alpha, mean - 3 * std, mean + 3 * std, alpha=0.3, color=COLORS[agent]
     )
@@ -29,7 +35,13 @@ agents = ["RHUCRL", "HUCRL", "baseline"]
 for agent in agents:
     df_ = df[df.agent == agent].sort_values("alpha")
     mean, std = df_["mean"], df_["std"]
-    ax[1].plot(alpha, mean, label=LABELS[agent], color=COLORS[agent])
+    ax[1].plot(
+        alpha,
+        mean,
+        label=LABELS[agent],
+        color=COLORS[agent],
+        linestyle=LINESTYLE[agent],
+    )
     ax[1].fill_between(
         alpha, mean - 3 * std, mean + 3 * std, alpha=0.3, color=COLORS[agent]
     )
@@ -44,7 +56,9 @@ agents = ["RHUCRL", "HUCRL", "baseline"]
 for agent in agents:
     df_ = df[df.agent == agent].sort_values("mass")
     mean, std = df_["mean"], df_["std"]
-    ax[2].plot(mass, mean, label=LABELS[agent], color=COLORS[agent])
+    ax[2].plot(
+        mass, mean, label=LABELS[agent], color=COLORS[agent], linestyle=LINESTYLE[agent]
+    )
     ax[2].fill_between(
         mass, mean - 3 * std, mean + 3 * std, alpha=0.3, color=COLORS[agent]
     )
