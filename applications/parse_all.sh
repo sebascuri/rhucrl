bf8dc4b9f8a57dd0a9e19cda86a57ef144cb9b97
scuri <sebastian.curi@inf.ethz.ch>
2021-02-03 14:24:49 +0100

    Add parse_all script

---
+++ applications/parse_all.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd action_robust
+python parse_arrl.py
+cp adversarial_robust*.json ..
+
+cd ../adversarial_rl
+python parse_adversarial_rl.py
+cp action_robust*.json ..
+
+cd domain_randomization
+python parse_dr.py
+cp parameter_robust*.json ..
